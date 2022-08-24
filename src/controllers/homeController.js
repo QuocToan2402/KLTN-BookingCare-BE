ca65ce605f4706c98b1fd4553110c5f87bdb7f3a
qtpham_dev <53518651+QuocToan2402@users.noreply.github.com>
2022-08-25 00:08:57 +0700

    connect to database mysql and get sample data to view.

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,15 @@
-let getHomePage = (req, res) => {
-	return res.render('homePage.ejs');
+import { json } from 'body-parser';
+import db from '../models/index';
+
+let getHomePage = async (req, res) => {
+	try {
+		let data = await db.User.findAll();
+		return res.render('homePage.ejs', {
+			data: JSON.stringify(data),
+		});
+	} catch (e) {
+		console.log(e);
+	}
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser'; //lay thong so tu client gui len server
 import viewEngine from './config/viewEngine';
 import initWebRoutes from './route/web';
+import connectToDB from './config/connectDB';
 require('dotenv').config();
 
 let app = express();
@@ -9,9 +10,12 @@ let app = express();
 //config app
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 viewEngine(app);
 //Khai bao cho express biet tat ca cac link dung trong initWebRoutes
 initWebRoutes(app);
+
+connectToDB();
 
 //port == undefined => port = 4000
 let port = process.env.PORT || 3000;
